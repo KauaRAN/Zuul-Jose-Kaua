95b1eb9370c7b5413de672648bf3a87360285d0e
CerqueiraNeto <125772487+CerqueiraNeto@users.noreply.github.com>
2023-08-30 22:14:59 -0300

    Update Command.java

--- Zuul-Jose-Kaua/src/Command.java
+++ Zuul-Jose-Kaua/src/Command.java
@@ -1,21 +1,7 @@
 /**
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
- *
- * This class holds information about a command that was issued by the user.
- * A command currently consists of two strings: a command word and a second
- * word (for example, if the command was "take map", then the two strings
- * obviously are "take" and "map").
- * 
- * The way this is used is: Commands are already checked for being valid
- * command words. If the user entered an invalid command (a word that is not
- * known) then the command word is <null>.
- *
- * If the command had only one word, then the second word is <null>.
- * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2016.02.29
- */
+ *  Essa classe irá "guardar" o comando oferecido pelo usuário. Consiste de duas palavras, sendo uma de ação e a outra a direção, objeto etc que 
+ *  faz jus a primeira palavra e sofre efeito dela.
+ **/
 
 public class Command
 {
@@ -23,11 +9,7 @@ public class Command
     private String secondWord;
 
     /**
-     * Create a command object. First and second word must be supplied, but
-     * either one (or both) can be null.
-     * @param firstWord The first word of the command. Null if the command
-     *                  was not recognised.
-     * @param secondWord The second word of the command.
+     * Construtor para definir os atributos.
      */
     public Command(String firstWord, String secondWord)
     {
@@ -36,9 +18,7 @@ public class Command
     }
 
     /**
-     * Return the command word (the first word) of this command. If the
-     * command was not understood, the result is null.
-     * @return The command word.
+     * Retorna a palavra de comando, ou seja, a primeira.
      */
     public String getCommandWord()
     {
@@ -46,8 +26,7 @@ public class Command
     }
 
     /**
-     * @return The second word of this command. Returns null if there was no
-     * second word.
+     * Retorna a segunda palavra.
      */
     public String getSecondWord()
     {
@@ -55,16 +34,13 @@ public class Command
     }
 
     /**
-     * @return true if this command was not understood.
+     * Métodos para retornar verdadeiro caso sejam os comandos.
      */
     public boolean isUnknown()
     {
         return (commandWord == null);
     }
 
-    /**
-     * @return true if the command has a second word.
-     */
     public boolean hasSecondWord()
     {
         return (secondWord != null);
