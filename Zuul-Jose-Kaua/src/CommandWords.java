e0a376e517bb9b610c24cc018bf501c15425010b
KauaRAN <142620519+KauaRAN@users.noreply.github.com>
2023-08-31 15:01:52 -0300

    Update CommandWords.java

--- Zuul-Jose-Kaua/src/CommandWords.java
+++ Zuul-Jose-Kaua/src/CommandWords.java
@@ -6,49 +6,8 @@ public class CommandWords
 {
     // a constant array that holds all valid command words
     private static final String[] validCommands = {
-        "ir", "sair", "ajuda","olhar","comer"
+        "ir", "sair", "ajuda", "olhar", "comer", "pegar"
     };
-
-    /**
-     * Constructor - initialise the command words.
-     */
-    public CommandWords()
-    {
-        // nothing to do at the moment...
-    }
-
-    /**
-     * Check whether a given String is a valid command word. 
-     * @return true if a given string is a valid command,
-     * false if it isn't.
-     */
-    public boolean isCommand(String aString)
-    {
-        for(int i = 0; i < validCommands.length; i++) {
-            if(validCommands[i].equals(aString))
-                return true;
-        }
-        // if we get here, the string was not found in the commands
-        return false;
-    }
-    /**
- * Fizemos uma pequena alteração na linha 8, mudando os comandos válidos para português.
- */
-
-public class CommandWords
-{
-    // a constant array that holds all valid command words
-    private static final String[] validCommands = {
-        "ir", "sair", "ajuda","ver"
-    };
-
-    /**
-     * Constructor - initialise the command words.
-     */
-    public CommandWords()
-    {
-        // nothing to do at the moment...
-    }
 
     /**
      * Check whether a given String is a valid command word. 
